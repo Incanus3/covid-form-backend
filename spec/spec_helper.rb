f8e7a9797ce77ffb8d66f040cdcd712bf5de5674
Jakub Kaláb <jakubkalab@gmail.com>
2020-10-03 13:01:24 +0200

    added simplecov configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,18 @@ ENV['APP_ENV'] = 'test'
 
 $LOAD_PATH.unshift(APP_ROOT)
 
-SimpleCov.start
+SimpleCov.start do
+  enable_coverage :branch
+
+  add_filter '/spec/'
+
+  add_group 'Application', 'app'
+  add_group 'Utilities',   'lib'
+
+  maximum_coverage_drop 3
+  minimum_coverage line: 95, branch: 65
+  minimum_coverage_by_file 90
+end
 
 require 'app/web/app'
 
